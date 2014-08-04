e09aa16d9778db87fa59436c716564ec4c0ab406
Inurion <inurion@live.com>
2014-08-04 18:07:27 -0300

    fixed log

--- README.md
+++ README.md
@@ -2,3 +2,20 @@ winston-postgre
 ===============
 
 A winston transporter for PostgreSQL database
+
+Installation
+------------
+
+
+
+
+Options
+------------
+
+
+Usage
+------------
+
+
+
+
--- test/winston-postgre-test.js
+++ test/winston-postgre-test.js
@@ -5,11 +5,12 @@ var db = require('../lib/winston-postgre');
 var options = {
   "connectionString" : "postgres://postgres:$skido10@localhost/teste",
   "schema" : "public",
-  "table" : "NodeLogs"
+  "table" : "nodelogs",
+  "createTable" : true
 };
 
 winston.add(winston.transports.PostgreSQL, options);
 
 
-winston.log('error', 'Just a simple test');
+winston.log('error', 'Just a simple test2');
 
